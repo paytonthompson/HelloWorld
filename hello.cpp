cec641cb325fbe391da19025eb049e9670cf85a1
Payton Thompson <paytonthompson79@gmail.com>
2020-12-23 14:37:56 -0600

    First repository made on my own.

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+using namespace std;
+int main () {
+string name; 
+cout << "Hello World" << endl;
+cout << "What is your name?" << endl;
+cin >> name;
+cout << "Hello " << name << endl;
+}
